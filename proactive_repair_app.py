4fd4a0cff829f7d24964fce22692d942e2f55d40
Ejiro Onose <ejiro10000@gmail.com>
2023-09-11 00:54:49 +0100

    Update proactive_repair_app.py

--- proactive_repair_app.py
+++ proactive_repair_app.py
@@ -5,6 +5,13 @@ from langchain.callbacks import get_openai_callback
 from langchain.chains import ConversationChain
 from langchain.chains.conversation.memory import ConversationSummaryMemory
 
+from streamlit_chat import message
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationalRetrievalChain
+from langchain.document_loaders.csv_loader import CSVLoader
+
+
 from dataclasses import dataclass
 from typing import Literal
 
