1876dda36ea1640db559f334edbca26d89c71aca
Ejiro Onose <ejiro10000@gmail.com>
2023-09-11 02:39:26 +0100

    Update using llamaindex proactive_repair_app.py

--- proactive_repair_app.py
+++ proactive_repair_app.py
@@ -1,144 +1,46 @@
 import streamlit as st
-import streamlit.components.v1 as components
+from llama_index import VectorStoreIndex, ServiceContext, Document
+from llama_index.llms import OpenAI
+import openai
+from llama_index import SimpleDirectoryReader
 
+openai.api_key = "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"
 
-
-#from langchain.chat_models import ChatOpenAI
-#from langchain.prompts.chat import (
-#    ChatPromptTemplate,
- #   SystemMessagePromptTemplate,
-  #  AIMessagePromptTemplate,
-   # HumanMessagePromptTemplate,)
-
-
-#from langchain.chains.conversation.memory import ConversationSummaryMemory
-#from langchain.chains import ConversationChain
-
-from streamlit_chat import message
-#from langchain.embeddings.openai import OpenAIEmbeddings
-#from langchain.chains import ConversationalRetrievalChain
-#from langchain.document_loaders.csv_loader import CSVLoader
-
-
-
-from dataclasses import dataclass
-from typing import Literal
-
-
-
-@dataclass
-class Message:
-    """Class for keeping track of a chat message."""
-    origin: Literal["customer", "pr pal"]
-    message: str
-
-def load_css():
-    with open("style/style.css", "r") as f:
-        css = f"<style>{f.read()}</style>"
-        st.markdown(css, unsafe_allow_html=True)
-
-# def initialize_session_state():
-#     if "history" not in st.session_state:
-#         st.session_state.history = []
-#     if "token_count" not in st.session_state:
-#         st.session_state.token_count = 0
-#     if "conversation" not in st.session_state:
-#         llm = ChatOpenAI(
-#             temperature=0,
-#             openai_api_key=st.secrets["openai_api_key"],
-#             model_name="text-davinci-003"
-#         )
-#         st.session_state.conversation = ConversationChain(
-#             llm=llm,
-#             memory=ConversationSummaryMemory(llm=llm),
-#         )
-
-def on_click_callback():
-   with get_openai_callback() as cb:
-#        human_prompt = st.session_state.human_prompt
-#        llm_response = st.session_state.conversation.run(
-#            human_prompt
-#        )
-        st.session_state.history.append(
-            Message("customer", human_prompt)
-        )
-        st.session_state.history.append(
-            Message("pr pal", llm_response)
-        )
-        st.session_state.token_count += cb.total_tokens
-
-load_css()
-# initialize_session_state()
-
-st.title("Proactive Repair Pal 👷‍♀️🛠️")
-st.subheader(" ",divider='rainbow')
+st.set_page_config(page_title="Proactive Repair Pal", page_icon="👷‍♀️🛠️", layout="centered", initial_sidebar_state="auto", menu_items=None)
+st.title("Proactive Repair Pal👷‍♀️🛠️")
+st.divider()
 st.markdown("Welcome to the Proactive Repair Pal Bot. I am a proactive repair chatbot that tracks user tasks/complaints with reference IDs, providing real-time updates for enhanced user experience and efficiency")
+         
+if "messages" not in st.session_state.keys(): # Initialize the chat messages history
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Ask me about your tasks!"}
+    ]
 
-chat_placeholder = st.container()
-prompt_placeholder = st.form("chat-form")
-#credit_card_placeholder = st.empty()
+@st.cache_resource(show_spinner=False)
+def load_data():
+    with st.spinner(text="Loading..."):
+        reader = SimpleDirectoryReader(input_dir="https://github.com/Joggyjagz7/test-proactive-chatbot/blob/main/jobs_since_2021_with_complaints.xlsx", recursive=True)
+        docs = reader.load_data()
+        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt=" "))
+        index = VectorStoreIndex.from_documents(docs, service_context=service_context)
+        return index
 
-with chat_placeholder:
-    for chat in st.session_state.history:
-        div = f"""
-<div class="chat-row 
-    {'' if chat.origin == 'ai' else 'row-reverse'}">
-    <img class="chat-icon" src="app/style/{
-        'worker.png' if chat.origin == 'pr pal' 
-                      else 'user.png'}"
-         width=32 height=32>
-    <div class="chat-bubble
-    {'ai-bubble' if chat.origin == 'ai' else 'human-bubble'}">
-        &#8203;{chat.message}
-    </div>
-</div>
-        """
-        st.markdown(div, unsafe_allow_html=True)
-    
-    for _ in range(3):
-        st.markdown("")
+index = load_data()
+# chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True, system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features.")
+chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
 
-with prompt_placeholder:
-    st.markdown("**Ask me**")
-    cols = st.columns((6, 1))
-    cols[0].text_input(
-        "How can I help you today?",
-        value=" ",
-        label_visibility="collapsed",
-        key="human_prompt",
-    )
-    cols[1].form_submit_button(
-        "Submit", 
-        type="primary", 
-        on_click=on_click_callback, 
-    )
+if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
 
-#credit_card_placeholder.caption(f"""
-#Used {st.session_state.token_count} tokens \n
-#Debug Langchain conversation: 
-#{st.session_state.conversation.memory.buffer}
-#""")
+for message in st.session_state.messages: # Display the prior chat messages
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
 
-components.html("""
-<script>
-const streamlitDoc = window.parent.document;
-
-const buttons = Array.from(
-    streamlitDoc.querySelectorAll('.stButton > button')
-);
-const submitButton = buttons.find(
-    el => el.innerText === 'Submit'
-);
-
-streamlitDoc.addEventListener('keydown', function(e) {
-    switch (e.key) {
-        case 'Enter':
-            submitButton.click();
-            break;
-    }
-});
-</script>
-""", 
-    height=0,
-    width=0,
-)
+# If last message is not from assistant, generate a new response
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = chat_engine.chat(prompt)
+            st.write(response.response)
+            message = {"role": "assistant", "content": response.response}
+            st.session_state.messages.append(message) # Add response to message history
