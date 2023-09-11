3b20f1f72d6dbef8a8ef9cd37a6206d714e429ca
Ejiro Onose <ejiro10000@gmail.com>
2023-09-11 01:01:16 +0100

    Update proactive_repair_app.py

--- proactive_repair_app.py
+++ proactive_repair_app.py
@@ -1,20 +1,22 @@
 import streamlit as st
 import streamlit.components.v1 as components
-from langchain.callbacks import get_openai_callback
 from langchain.chains import ConversationChain
-from langchain.chains.conversation.memory import ConversationSummaryMemory
-
-from streamlit_chat import message
-from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
-from langchain.chains import ConversationalRetrievalChain
-from langchain.document_loaders.csv_loader import CSVLoader
-
 from langchain.prompts.chat import (
     ChatPromptTemplate,
     SystemMessagePromptTemplate,
     AIMessagePromptTemplate,
     HumanMessagePromptTemplate,
+
+
+from langchain.chains.conversation.memory import ConversationSummaryMemory
+
+from streamlit_chat import message
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.chains import ConversationalRetrievalChain
+from langchain.document_loaders.csv_loader import CSVLoader
+
+
 )
 
 
